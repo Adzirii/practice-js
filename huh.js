7504b8a17aa2d1d7f14d4523a5751ccd1f3afdee
adziri <liubov.nikita@gmail.com>
2024-04-06 14:32:28 +0200

    game logic added

--- huh.js
+++ huh.js
@@ -1,12 +1,50 @@
-const a = 5;
-const b = 10;
+const sing = {
+    0: "rock",
+    1: "paper",
+    2: "scissors"
+};
 
-function add() {
-  return c;
+function getComputerChoice() {
+
+
+    let rnd = Math.floor(Math.random() * 4);
+    return sing[rnd];
 }
 
-function print() {
-  add();
+console.log("Comp choise: " + getComputerChoice());
+
+let playerChoise = prompt("Write your sign: ").toLowerCase();
+
+console.log("Player choise: " + playerChoise);
+
+
+function playRound(playerChoise, computerChoice){
+   
+    switch(playerChoise){
+        case "rock":
+            if (computerChoice == sing[0])
+                return "Tie";
+            else if (computerChoice == sing[1])
+                return "Player lose";
+            else 
+                return "Player won";
+            case "paper":
+            if (computerChoice == sing[0])
+                return "Player won";
+            else if (computerChoice == sing[1])
+                return "Tie";
+            else 
+                return "Player lose";
+            case "scissors":
+            if (computerChoice == sing[0])
+                return "Player lose";
+            else if (computerChoice == sing[1])
+                return "Player won";
+            else 
+                return "Tie";
+            default:
+                return "Ivalid sing";
+    }
 }
 
-print();
+console.log(playRound(playerChoise, getComputerChoice()));
