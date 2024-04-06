2de926b61ebf2f38dccc6578a8927a9d4625cc35
adziri <liubov.nikita@gmail.com>
2024-04-06 14:48:58 +0200

    game logic added

--- huh.js
+++ huh.js
@@ -5,21 +5,15 @@ const sing = {
 };
 
 function getComputerChoice() {
-
-
-    let rnd = Math.floor(Math.random() * 4);
+    let rnd = Math.floor(Math.random() * 3);
     return sing[rnd];
 }
 
-console.log("Comp choise: " + getComputerChoice());
-
-let playerChoise = prompt("Write your sign: ").toLowerCase();
-
-console.log("Player choise: " + playerChoise);
 
 
 function playRound(playerChoise, computerChoice){
-   
+        console.log("Comp choise: " + computerChoice);
+        console.log("Player choise: " + playerChoise);
     switch(playerChoise){
         case "rock":
             if (computerChoice == sing[0])
@@ -33,7 +27,7 @@ function playRound(playerChoise, computerChoice){
                 return "Player won";
             else if (computerChoice == sing[1])
                 return "Tie";
-            else 
+            else  
                 return "Player lose";
             case "scissors":
             if (computerChoice == sing[0])
@@ -47,4 +41,21 @@ function playRound(playerChoise, computerChoice){
     }
 }
 
-console.log(playRound(playerChoise, getComputerChoice()));
+
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i< 5; i++){
+        let playerChoise = prompt("Write your sign: ").toLowerCase();        
+        console.log("\n");
+        let res = playRound(playerChoise, getComputerChoice());
+        console.log(res);
+        if (res === "Player won") playerScore++;
+        else if (res === "Player lose") computerScore++;
+        console.log(`Score: player - ${playerScore} , comuter - ${computerScore}`);
+    }
+}
+
+playGame();
+
+
